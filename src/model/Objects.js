10bc3d7f0b23ce8df762d98950dcfc31e07d56e0
aviendaaiel <pinkladydancer1981@hotmail.com>
2017-07-19 20:58:54 +0100

    partial success

--- src/model/Objects.js
+++ src/model/Objects.js
@@ -3,6 +3,6 @@
     name = prompt("What is your name?");
                     localStorage.setItem("name", name);
 
-
+                    var index = "Hello, and welcome to my test page. Hopefully you have just been asked to enter your name. We are happy to have you " + name;
 
 
--- src/model/page2.js
+++ src/model/page2.js
@@ -1,3 +1,12 @@
 // JavaScript source code
     name = localStorage.getItem("name");
-      
+
+var damage = math.floor((math.random() * 10) + 1);
+if (damage <= 5){
+    var nextPage = "ohnoes.html";
+}
+else {
+    var nextPage = "kickass.html";
+}
+
+var fight = "You dealt " + damage + " to the enemy."
