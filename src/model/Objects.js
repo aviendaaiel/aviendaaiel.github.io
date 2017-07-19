d106080bf6b4507b0cb68294030dfc355d103638
aviendaaiel <pinkladydancer1981@hotmail.com>
2017-07-19 19:52:08 +0100

    Initial commit

---
+++ src/model/Objects.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+        alert("Hi, Stranger!");
+    name = prompt("What is your name?");
+                    localStorage.setItem("name", name);
+
+
+
+
---
+++ src/model/page2.js
@@ -0,0 +1,3 @@
+// JavaScript source code
+    name = localStorage.getItem("name");
+        alert("Hi, " + name);
