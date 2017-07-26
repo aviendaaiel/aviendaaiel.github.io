578d0441aa2ddcb24b11ac78a392cf481c236d5e
aviendaaiel <pinkladydancer1981@hotmail.com>
2017-07-26 19:50:10 +0100

    Gangs of Arcadia

---
+++ src/fight.js
@@ -0,0 +1,296 @@
+// Function for all fights
+
+function fight(characterName, hp, pokemonhp, nextPage) {
+
+    if (characterName == "Fortuna") {
+        var damage = Math.floor((Math.random() * 10) + 1);
+    }
+    else if (characterName == "Smacka") {
+        var damage = Math.floor((Math.random() * 12) + 1);
+    }
+    else if (characterName == "Interra") {
+        var damage = Math.floor((Math.random() * 7) + 1);
+    }
+
+    var pokemonDamage = Math.floor((Math.random() * 6) + 1);
+    pokemonhp = pokemonhp - damage;
+
+    hp = hp - pokemonDamage;
+
+    if (hp < 0) {
+        hp = 0;
+    }
+
+    if (pokemonhp < 0) {
+        pokemonhp = 0;
+    }
+
+    localStorage.setItem("pokemonhp", pokemonhp);
+    localStorage.setItem("hp", hp);
+    localStorage.setItem("pokemonDamage", pokemonDamage);
+    localStorage.setItem("damage", damage);
+
+    console.log("You are playing as " + characterName);
+    console.log("You have " + hp + " HP remaining");
+    console.log("You dealt " + damage + " damage");
+    console.log("Enemy HP: " + pokemonhp);
+    console.log("Enemy damage dealt: " + pokemonDamage);
+
+    if (hp == 0) {
+        window.location.href = "youdied.html";
+    }
+    else if (pokemonhp == 0) {
+        window.location.href = "youwon.html";
+    }
+    else {
+        window.location.href = nextPage;
+    }
+}
+
+//display player stats
+
+function playerStats(characterName, hp) {
+
+    characterName = localStorage.getItem("characterName");
+    hp = localStorage.getItem("hp");
+
+    console.clear();
+    console.log("You are playing as " + characterName);
+    console.log(characterName + " has " + hp + " HP remaining");
+}
+
+//Game start
+
+function gameStart() {
+
+    localStorage.clear();
+
+    var gameContent = document.getElementById("gameContent");
+
+    var playAsSmacka = document.getElementById("playAsSmacka").addEventListener("click", function () {
+
+        play("Smacka");
+        
+    });
+
+    var playAsFortuna = document.getElementById("playAsFortuna").addEventListener("click", function () {
+
+        play("Fortuna");
+
+    });
+
+    var playAsInterra = document.getElementById("playAsInterra").addEventListener("click", function () {
+
+        play("Interra");
+ 
+    });
+
+     
+}
+
+
+//Meet the Chief of Police
+
+function play(characterName) {
+
+    if (characterName == "Smacka") {
+
+        localStorage.setItem("characterName", characterName);
+
+        gameContent.innerHTML = "<p>You begin your adventure in the office of the Chief of Police, Gordon Freeman.</p> \
+            <div class='speech'><p>&#10077;You are my only hope Smacka, you came highly recommended from the Mayor of Fruitopolis. I hear you did an awesomesauce job with those bad apples.&#10078;</p> \
+            <p>&#10077; Meh, that was an easy job. I prefer a challenge.&#10078;</p> \
+            <p>&#10077; Good! Here, this is the list of people I want you to rid me of. If you fail, your name will be mud so don&#10076;t f** k it up!! &#10078;</p> \
+            <p>&#10077; Chill! Give me a few days and your troubles will be down the u- bend.&#10078;</p ></div> \
+            <p>As you shut the door behind you, you roll your eyes. This is gonna be a piece of cake!</p> \
+            <a href='#' id='exploreArcadia'>Explore the town</a> <br /><br/> \
+            <a href='#' id='visitBar'>Visit Shodans Bar</a> <br /><br/> \
+            <a href='#' id='readList'>Read the list of names</a>";
+
+        var hp = 60;
+
+        localStorage.setItem("hp", hp);
+    }
+
+    else if (characterName == "Fortuna") {
+
+        localStorage.setItem("characterName", characterName);
+
+        gameContent.innerHTML = "<p>You begin your adventure in the office of the Chief of Police, Gordon Freeman.</p> \
+        <p>&#10077;You are my only hope Fortuna, you came highly recommended from the Mayor of Fruitopolis. I hear you did an awesomesauce job with those bad apples.&#10078; </p > \
+        <p>&#10077;Meh, I got lucky.Just the roll of a dice.&#10078;</p> \
+        <p>&#10077;I hope your luck holds out.Here, this is the list of people I want you to rid me of.If you fail, your name will be mud so don&#10076;t f** k it up!!&#10078;</p> \
+        <p>&#10077;Easy, easy! Give me a few days and your troubles will be down the u-bend.&#10078;</p > \
+        <p>As you shut the door behind you, you wonder how this town got so unlucky! </p> \
+        <a href='#' id='exploreArcadia'>Explore the town</a> <br /><br/> \
+        <a href='#' id='visitBar'>Visit Shodans Bar</a> <br /><br/> \
+        <a href='#' id='readList'>Read the list of names</a>";
+
+        var hp = 50;
+
+        localStorage.setItem("hp", hp);
+    }
+
+    else {
+
+        localStorage.setItem("characterName", characterName);
+
+        gameContent.innerHTML = "<p>You begin your adventure in the office of the Chief of Police, Gordon Freeman.</p> \
+        <p>&#10077;You are my only hope Interra, you came highly recommended from the Mayor of Fruitopolis. I hear you did an awesomesauce job with those bad apples.&#10078; </p > \
+        <p>&#10077;I assure you it was all meticulously planned and executed.&#10078;</p> \
+        <p>&#10077;As it should be. Here, this is the list of people I want you to rid me of. If you fail, your name will be mud so don&#10076;t f** k it up!!&#10078;</p> \
+        <p>&#10077;Don&#10076;t worry Sir! Give me a few days and your troubles will be down the u-bend.&#10078;</p > \
+        <p>As you shut the door behind you, you wonder how this town survives! </p> \
+        <a href='#' id='exploreArcadia'>Explore the town</a> <br /><br/> \
+        <a href='#' id='visitBar' > Visit Shodans Bar</a > <br /><br/> \
+        <a href='#' id='readList'>Read the list of names</a>";
+
+        var hp = 55;
+
+        localStorage.setItem("hp", hp);
+
+    };
+
+    playerStats();
+
+    var exploreArcadia = document.getElementById("exploreArcadia").addEventListener("click", function () {
+
+        exploreTown();
+
+    });
+
+    var visitBar = document.getElementById("visitBar").addEventListener("click", function () {
+
+        visitShodans();
+
+    });
+
+    var readList = document.getElementById("readList").addEventListener("click", function () {
+
+        readNameList();
+
+    });
+
+}
+
+//Explore Arcadia
+
+function exploreTown(characterName) {
+
+    characterName = localStorage.getItem("characterName");
+
+    playerStats();
+
+    if (characterName == "Smacka") {
+
+        gameContent.innerHTML = '<p>I&#10076;m ' + characterName + '</p> \
+        <a href="#" id="visitBar">Take a look at Shodans bar</a> <br /><br/> \
+        <a href="#" id="readList">Read the list of names</a>';
+
+    }
+
+    else if (characterName == "Fortuna") {
+
+        gameContent.innerHTML = '<p>No casino .....</p> \
+        <a href="#" id="visitBar">Take a look at Shodans bar</a> <br /><br/> \
+        <a href="#" id="readList">Read the list of names</a>';
+
+    }
+
+    else {
+
+        gameContent.innerHTML = '<p>I wonder if there is a library ....</p> \
+        <a href="#" id="visitBar">Take a look at this bar</a> <br /><br/> \
+        <a href="#" id="readList">Read the list of names</a>';
+
+    };
+
+
+    var visitBar = document.getElementById("visitBar").addEventListener("click", function () {
+
+        visitShodans();
+
+    });
+
+    var readList = document.getElementById("readList").addEventListener("click", function () {
+
+        readNameList();
+
+    });
+
+}
+
+//Visit Shodans Bar
+
+function visitShodans(characterName) {
+
+    characterName = localStorage.getItem("characterName");
+
+    playerStats();
+
+    if (characterName == "Smacka") {
+
+        gameContent.innerHTML = '<p>Proper shithole this is!</p> \
+        <a href="#" id="exploreArcadia">See what the rest of the town has to offer</a> <br /><br/> \
+        <a href="#" id="readList">Read the list of names</a>';
+
+    }
+
+    else if (characterName == "Fortuna") {
+
+        gameContent.innerHTML = '<p>Oh God, what on earth is that smell!!</p> \
+        <a href="#" id="exploreArcadia">See what the rest of the town has to offer</a> <br /><br/> \
+        <a href="#" id="readList">Read the list of names</a>';
+
+    }
+
+    else {
+
+        gameContent.innerHTML = '<p>Oh! A bookshelf!</p> \
+        <a href="#" id="exploreArcadia">See what the rest of the town has to offer</a> <br /><br/> \
+        <a href="#" id="readList">Read the list of names</a>';
+
+    };
+
+
+    var exploreArcadia = document.getElementById("exploreArcadia").addEventListener("click", function () {
+
+        exploreTown();
+
+    });
+
+    var readList = document.getElementById("readList").addEventListener("click", function () {
+
+        readNameList();
+
+    });
+
+}
+
+//Read the list of targets
+
+function readNameList(characterName) {
+
+    characterName = localStorage.getItem("characterName");
+
+    playerStats();
+
+    gameContent.innerHTML = '<div class="deathList"><h3><u>Deathlist!</u></h3><p>The Nameless One</p> \
+    <p>Lara Croft</p><p>Link</p><p>Guybrush Threepwood</p><p>Master Chief</p><p>Sephiroth</p><p>GLaDOS</p> \
+    <p>Kratos</p><p>Brucie Kibbutz</p></div> \
+    <a href="#" id="exploreArcadia">See what the rest of the town has to offer</a> <br /><br/> \
+    <a href="#" id="visitBar2">Go back to the Bar and decide who to track first</a>';
+
+    var exploreArcadia = document.getElementById("exploreArcadia").addEventListener("click", function () {
+
+        exploreTown();
+
+    });
+
+    var visitBar2 = document.getElementById("visitBar2").addEventListener("click", function () {
+
+        visitShodans2();
+
+    });
+
+}
--- src/model/Objects.js
+++
@@ -1,8 +0,0 @@
-// JavaScript source code
-        alert("Hi, Stranger!");
-    name = prompt("What is your name?");
-                    localStorage.setItem("name", name);
-
-                    var index = "Hello, and welcome to my test page. Hopefully you have just been asked to enter your name. We are happy to have you " + name;
-
-
