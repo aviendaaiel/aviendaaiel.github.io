dd6887e325b7d429c087548b42b6353dc7bc357d
aviendaaiel <pinkladydancer1981@hotmail.com>
2017-08-07 21:26:44 +0100

    Added content

--- src/fight.js
+++ src/fight.js
@@ -100,12 +100,11 @@ function play(characterName) {
         gameContent.innerHTML = "<p>You begin your adventure in the office of the Chief of Police, Gordon Freeman.</p> \
             <div class='speech'><p>&#10077;You are my only hope Smacka, you came highly recommended from the Mayor of Fruitopolis. I hear you did an awesomesauce job with those bad apples.&#10078;</p> \
             <p>&#10077; Meh, that was an easy job. I prefer a challenge.&#10078;</p> \
-            <p>&#10077; Good! Here, this is the list of people I want you to rid me of. If you fail, your name will be mud so don&#10076;t f** k it up!! &#10078;</p> \
+            <p>&#10077; Good! Here, this is the list of people I want you to rid me of. If you fail, your name will be mud so don&#39;t f** k it up!! &#10078;</p> \
             <p>&#10077; Chill! Give me a few days and your troubles will be down the u- bend.&#10078;</p ></div> \
             <p>As you shut the door behind you, you roll your eyes. This is gonna be a piece of cake!</p> \
             <div class='choices'><a href='#' id='exploreArcadia'>Explore the town</a> <br /><br/> \
-            <a href='#' id='visitBar'>Visit Shodans Bar</a> <br /><br/> \
-            <a href='#' id='readList'>Read the list of names</a></div>";
+            <a href='#' id='readList'>Read the list of names the Police Chief gave you</a></div>";
 
         var hp = 60;
 
@@ -119,12 +118,11 @@ function play(characterName) {
         gameContent.innerHTML = "<p>You begin your adventure in the office of the Chief of Police, Gordon Freeman.</p> \
         <p>&#10077;You are my only hope Fortuna, you came highly recommended from the Mayor of Fruitopolis. I hear you did an awesomesauce job with those bad apples.&#10078; </p > \
         <p>&#10077;Meh, I got lucky.Just the roll of a dice.&#10078;</p> \
-        <p>&#10077;I hope your luck holds out.Here, this is the list of people I want you to rid me of.If you fail, your name will be mud so don&#10076;t f** k it up!!&#10078;</p> \
+        <p>&#10077;I hope your luck holds out.Here, this is the list of people I want you to rid me of.If you fail, your name will be mud so don&#39;t f** k it up!!&#10078;</p> \
         <p>&#10077;Easy, easy! Give me a few days and your troubles will be down the u-bend.&#10078;</p > \
         <p>As you shut the door behind you, you wonder how this town got so unlucky! </p> \
         <div class='choices'><a href='#' id='exploreArcadia'>Explore the town</a> <br /><br/> \
-        <a href='#' id='visitBar'>Visit Shodans Bar</a> <br /><br/> \
-        <a href='#' id='readList'>Read the list of names</a></div>";
+        <a href='#' id='readList'>Read the list of names the Police Chief gave you</a></div>";
 
         var hp = 50;
 
@@ -138,12 +136,11 @@ function play(characterName) {
         gameContent.innerHTML = "<p>You begin your adventure in the office of the Chief of Police, Gordon Freeman.</p> \
         <p>&#10077;You are my only hope Interra, you came highly recommended from the Mayor of Fruitopolis. I hear you did an awesomesauce job with those bad apples.&#10078; </p > \
         <p>&#10077;I assure you it was all meticulously planned and executed.&#10078;</p> \
-        <p>&#10077;As it should be. Here, this is the list of people I want you to rid me of. If you fail, your name will be mud so don&#10076;t f** k it up!!&#10078;</p> \
+        <p>&#10077;As it should be. Here, this is the list of people I want you to rid me of. If you fail, your name will be mud so don&#39;t f** k it up!!&#10078;</p> \
         <p>&#10077;Don&#10076;t worry Sir! Give me a few days and your troubles will be down the u-bend.&#10078;</p > \
         <p>As you shut the door behind you, you wonder how this town survives! </p> \
         <div class='choices'><a href='#' id='exploreArcadia'>Explore the town</a> <br /><br/> \
-        <a href='#' id='visitBar'>Visit Shodans Bar</a> <br /><br/> \
-        <a href='#' id='readList'>Read the list of names</a></div>";
+        <a href='#' id='readList'>Read the list of names the Police Chief gave you</a></div>";
 
         var hp = 55;
 
@@ -156,12 +153,6 @@ function play(characterName) {
     var exploreArcadia = document.getElementById("exploreArcadia").addEventListener("click", function () {
 
         exploreTown();
-
-    });
-
-    var visitBar = document.getElementById("visitBar").addEventListener("click", function () {
-
-        visitShodans();
 
     });
 
@@ -181,45 +172,14 @@ function exploreTown(characterName) {
 
     playerStats();
 
-    if (characterName == "Smacka") {
-
-        gameContent.innerHTML = '<p>You start wandering around the town, it&#39;s a lot worse than you expected! \
+        gameContent.innerHTML = '<p>You start wandering around the town, it&#39;s pretty much as bad as you expected! \
         The place is so run down, it&#39;s such a dump. You pass several boarded up shops with broken glass outside them. \
         Blood stains the pavement and no one makes any eye contact with you. You hear some shouting up ahead, and being a nosy bastard you go and see what&#39;s going on. </p>\
-        It doesn&#39t turn out to be anything interesting though, just two local drunks having a barney and a few also drunk locals cheering them on. \
-        >p>You overhear one of them mentioning a bar, and given the state of the locals you reckon that&#39;ll be a drinking hole for the scummier residents of the town. \
+        <p>It doesn&#39t turn out to be anything interesting though, just two local drunks having a barney and a few (also drunk) locals cheering them on. \
+        You overhear one of them mentioning a bar on Asteroid Street, and given the state of the locals you reckon that&#39;ll be a drinking hole for the scummier residents of the town. \
         Probably as good a place to start as any!</p> \
-        <div class="choices"><a href="#" id="visitBar">Take a look at Shodans bar</a> <br /><br/> \
-        <a href="#" id="readList">Read the list of names</a></div>';
-
-    }
-
-    else if (characterName == "Fortuna") {
-
-        gameContent.innerHTML = '<p>You start wandering around the town, it&#39;s a lot worse than you expected! \
-        The place is so run down, it&#39;s such a dump. You pass several boarded up shops with broken glass outside them. \
-        Blood stains the pavement and no one makes any eye contact with you. You hear some shouting up ahead, and being a nosy bastard you go and see what&#39;s going on. </p>\
-        It doesn&#39t turn out to be anything interesting though, just two local drunks having a barney and a few also drunk locals cheering them on. \
-        >p>You overhear one of them mentioning a bar, and given the state of the locals you reckon that&#39;ll be a drinking hole for the scummier residents of the town. \
-        Probably as good a place to start as any!</p> \
-        <div class="choices"><a href="#" id="visitBar">Take a look at Shodans bar</a> <br /><br/> \
-        <a href="#" id="readList">Read the list of names</a></div>';
-
-    }
-
-    else {
-
-        gameContent.innerHTML = '<p>You start wandering around the town, it&#39;s a lot worse than you expected! \
-        The place is so run down, it&#39;s such a dump. You pass several boarded up shops with broken glass outside them. \
-        Blood stains the pavement and no one makes any eye contact with you. You hear some shouting up ahead, and being a nosy bastard you go and see what&#39;s going on. </p>\
-        It doesn&#39t turn out to be anything interesting though, just two local drunks having a barney and a few also drunk locals cheering them on. \
-        >p>You overhear one of them mentioning a bar, and given the state of the locals you reckon that&#39;ll be a drinking hole for the scummier residents of the town. \
-        Probably as good a place to start as any!</p> \
-        <div class="choices"><a href="#" id="visitBar">Take a look at Shodans bar</a> <br /><br/> \
-        <a href="#" id="readList">Read the list of names</a></div>';
-
-    };
-
+        <div class="choices"><a href="#" id="visitBar">Take a look at the bar</a> <br /><br/> \
+        <a href="#" id="readList">Read the list of names the Police Chief gave you</a></div>';
 
     var visitBar = document.getElementById("visitBar").addEventListener("click", function () {
 
@@ -243,14 +203,16 @@ function visitShodans(characterName) {
 
     playerStats();
 
-        gameContent.innerHTML = '<p>There&#10076;s a sign on the door saying it doesn&#10076;t open till 6. \
-        Guess you&#10076;ll just have to come back later.</p> \
-        <div class="choices"><a href="#" id="exploreArcadia">See what the rest of the town has to offer</a> <br /><br/> \
-        <a href="#" id="readList">Read the list of names</a></div>';
+    gameContent.innerHTML = '<p>About 200yds down Asteroid Street you spot a dark building in desperate need of a paint job. \
+        Shodans is a pretty ordinary looking bar, one of the windows is smashed and the pavement outside is covered in cigarette butts. \
+        Once you get closer you see there&#39;s a sign on the door saying it doesn&#39;t open till 6pm. \
+        Guess you&#39;ll just have to come back later.</p> \
+        <div class="choices"><a href="#" id="exploreArcadia2">Continue exploring Arcadia</a> <br /><br/> \
+        <a href="#" id="readList">Read the list of names the Police Chief gave you</a></div>';
 
-    var exploreArcadia = document.getElementById("exploreArcadia").addEventListener("click", function () {
+    var exploreArcadia2 = document.getElementById("exploreArcadia2").addEventListener("click", function () {
 
-        exploreTown();
+        exploreTown2();
 
     });
 
@@ -273,26 +235,14 @@ function readNameList(characterName) {
     gameContent.innerHTML = '<div class="deathList"><h3><u>Deathlist!</u></h3><p>The Nameless One</p> \
     <p>Gannondorf</p><p>Bowser</p><p>Kefka</p><p>Porky Minch</p><p>Sephiroth</p><p>GLaDOS</p> \
     <p>Kratos</p><p>Handsome Jack</p></div><br/> \
-    <div class="choices"><a href="#" id="exploreArcadia">See what the rest of the town has to offer</a> <br /><br/> \
-    <a href="#" id="visitBar">Take a look at Shodans bar</a> <br /><br/></div>';
-    /*<a href="#" id="visitBar2">Go back to the Bar and decide who to track first</a>*/
+    <div class="choices"><a href="#" id="visitBar2">Go back to the Bar and decide who to track first</a></div>';
 
-    var exploreArcadia = document.getElementById("exploreArcadia").addEventListener("click", function () {
 
-        exploreTown();
 
-    });
-
-    var visitBar = document.getElementById("visitBar").addEventListener("click", function () {
-
-        visitShodans();
-
-    });
-
-    /*var visitBar2 = document.getElementById("visitBar2").addEventListener("click", function () {
+    var visitBar2 = document.getElementById("visitBar2").addEventListener("click", function () {
 
         visitShodans2();
 
-    });*/
+    });
 
 }
